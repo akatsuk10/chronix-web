53f4d739b7580db15f203fd5cabfc933ba24ac45
Shivendra Mishra <shivendramishra.sm93@gmail.com>
2025-06-28 22:36:21 +0530

    Fixed prisma

--- app/api/waitlist/route.ts
+++ app/api/waitlist/route.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@/app/generated/prisma';
+import { PrismaClient } from '@/app/prisma';
 import { NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
